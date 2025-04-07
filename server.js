5cb8ab5e96253568819277d5c562a32048310bd7
Zeeshan Riasat <link.zeeshan.riasat@gmail.com>
2025-04-07 16:59:10 +0500

    Read voice using system mic and manipulate voice audio

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3003;
+
+// Serve static files from the public directory
+app.use(express.static("public"));
+
+// Serve the main page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
